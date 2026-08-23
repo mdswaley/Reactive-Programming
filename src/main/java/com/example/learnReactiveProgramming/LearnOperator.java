5b6d88d3a7be3d583a6a6c9697f37d2e34fd92ba
mdswaley <mdswaley075@gmail.com>
2026-08-23 12:05:05 +0530

    callable from Mono and @SneakyThrows

--- src/main/java/com/example/learnReactiveProgramming/LearnOperator.java
+++ src/main/java/com/example/learnReactiveProgramming/LearnOperator.java
@@ -1,9 +1,11 @@
 package com.example.learnReactiveProgramming;
 
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -21,5 +23,16 @@ public class LearnOperator {
                 (tick) -> log.info("Ticks : {}", tick) // tell to the producer give all data in every second
         );
 
+        Mono<String> fromCallable = Mono.fromCallable(
+                () -> slowTask()
+        );
+
+    }
+
+    @SneakyThrows // @SneakyThrows is a Lombok annotation used to avoid explicitly handling checked exceptions.
+//    without this you either need to add try-catch or throws in method level
+    private String slowTask(){
+        Thread.sleep(4000);
+        return "some Data";
     }
 }
