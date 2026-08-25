b7bffd01782d055c3813a4e355b7ab9c8c08a3c6
mdswaley <mdswaley075@gmail.com>
2026-08-25 11:12:12 +0530

    not able to catch error using try-catch

--- src/main/java/com/example/learnReactiveProgramming/LearnError.java
+++ src/main/java/com/example/learnReactiveProgramming/LearnError.java
@@ -8,12 +8,19 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class LearnError {
     public void learnErrorHandling(){
-        Mono<String> fromCallable = Mono.fromCallable(()->{
-            Thread.sleep(4000);
-            throw new RuntimeException("error occur");
-        });
+//        after using try-catch not able to catch the exception
+//        bcz after 4s long-running stream the try-catch already gone
+        try {
+            Mono<String> fromCallable = Mono.fromCallable(()->{
+                Thread.sleep(4000);
+                throw new RuntimeException("error occur");
+            });
 
-        fromCallable.subscribe(data -> log.info("data : {}",data));
+            fromCallable.subscribe(data -> log.info("data : {}",data));
+        }catch (Exception e){
+            log.error("Caught: ", e);
+        }
+
     }
 
 }
