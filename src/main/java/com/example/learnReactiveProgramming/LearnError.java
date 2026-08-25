bdd61e9cead29011c67659efb1b77cd8d9db08bd
mdswaley <mdswaley075@gmail.com>
2026-08-25 11:18:58 +0530

    onErrorComplete method in Mono

--- src/main/java/com/example/learnReactiveProgramming/LearnError.java
+++ src/main/java/com/example/learnReactiveProgramming/LearnError.java
@@ -10,17 +10,31 @@ public class LearnError {
     public void learnErrorHandling(){
 //        after using try-catch not able to catch the exception
 //        bcz after 4s long-running stream the try-catch already gone
-        try {
-            Mono<String> fromCallable = Mono.fromCallable(()->{
-                Thread.sleep(4000);
-                throw new RuntimeException("error occur");
-            });
+//        try {
+//            Mono<String> fromCallable = Mono.fromCallable(()->{
+//                Thread.sleep(4000);
+//                throw new RuntimeException("error occur");
+//            });
+//
+//            fromCallable.subscribe(data -> log.info("data : {}",data));
+//        }catch (Exception e){
+//            log.error("Caught: ", e);
+//        }
 
-            fromCallable.subscribe(data -> log.info("data : {}",data));
-        }catch (Exception e){
-            log.error("Caught: ", e);
-        }
 
+        Mono<String> fromCallable = Mono.fromCallable(()->{
+            Thread.sleep(4000);
+            throw new RuntimeException("error occur");
+        });
+
+        fromCallable
+                .onErrorComplete() // simple ignore the error and continue
+                .subscribe(
+                        data -> log.info("data : {}",data)
+                );
+
+        log.info("After callable"); // this will return after 4s. Bcz we are still using synchronous programming only main thread is running.
+        // And it was blocked in thread sleep for 4s
     }
 
 }
