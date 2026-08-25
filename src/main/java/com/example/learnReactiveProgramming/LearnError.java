e6bf051acd9620733689fd018566b0168584d38a
mdswaley <mdswaley075@gmail.com>
2026-08-25 11:30:34 +0530

    onErrorResume method explain using flux example

--- src/main/java/com/example/learnReactiveProgramming/LearnError.java
+++ src/main/java/com/example/learnReactiveProgramming/LearnError.java
@@ -2,6 +2,7 @@ package com.example.learnReactiveProgramming;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -33,11 +34,20 @@ public class LearnError {
 //                        data -> log.info("data : {}",data)
 //                );
 
-        fromCallable
-                .onErrorReturn("default fallback") // this will return default value for handling error
-                .subscribe(
-                        data -> log.info("data: {}", data)
-                );
+//        fromCallable
+//                .onErrorReturn("default fallback") // this will return default value for handling error
+//                .subscribe(
+//                        data -> log.info("data: {}", data)
+//                );
+
+        Flux.just("apple", "banana", "cherry")
+                .map(fruit -> {
+                    if (fruit.equals("banana")) throw new RuntimeException("Bad fruit!");
+                    return fruit.toUpperCase();
+                })
+                .onErrorResume(err -> Flux.just("mango", "orange")) //  Falling Back to Another Stream
+                .subscribe(System.out::println);
+
 
         log.info("After callable"); // this will return after 4s. Bcz we are still using synchronous programming only main thread is running.
         // And it was blocked in thread sleep for 4s
